38b93285cbe6b683e7156caed371b0e138557331
dodeca12 <dev@sgorantla.ca>
2021-06-17 21:48:26 -0400

    Created zalgo.cc file

---
+++ zalgo.cc
@@ -0,0 +1,25 @@
+#include "zalgo.h"
+
+#include <stdlib.h>
+#include <string>
+#include <random>
+
+Zalgo::Zalgo()
+{
+
+}
+
+Zalgo::~Zalgo()
+{
+
+}
+
+std::string Zalgo::getDiacritic(int position, int repeatCount)
+{
+    
+}
+
+std::string Zalgo::generateLine(std::string input, int aboveCount, int middleCount, int belowCont)
+{
+
+}

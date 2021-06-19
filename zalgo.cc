6bee85d4e1e8b6eb98b0e73a55664636551fe7af
dodeca12 <dev@sgorantla.ca>
2021-06-18 23:45:00 -0400

    Implemented basic generateLine function logic

--- zalgo.cc
+++ zalgo.cc
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 #include <string>
-#include <random>
+#include <time.h>
 
 Zalgo::Zalgo()
 {
@@ -16,10 +16,45 @@ Zalgo::~Zalgo()
 
 std::string Zalgo::getDiacritic(int position, int repeatCount)
 {
-    
+    std::string glyphWithDiacritic = "";
+    srand(time(NULL));
+
+    if(position == 0)
+    {
+        for (int i = 0; i < repeatCount; i++)
+        {
+            glyphWithDiacritic += this->ABOVE_DIACRITICS[rand() % 50];
+        }
+    }
+    else if(position == 1)
+    {
+        for (int i = 0; i < repeatCount; i++)
+        {
+            glyphWithDiacritic += this->MIDDLE_DIACRITICS[rand() % 23];
+        }
+    }
+    else if(position == 3)
+    {
+        for(int i = 0; i < repeatCount; i++)
+        {
+            glyphWithDiacritic += this->BELOW_DIACRITICS[rand() % 40];
+        }
+    }
+
+    return glyphWithDiacritic;
 }
 
-std::string Zalgo::generateLine(std::string input, int aboveCount, int middleCount, int belowCont)
+std::string Zalgo::generateLine(std::string input, int aboveCount, int middleCount, int belowCount)
 {
 
+    std::string zalgoLine = "";
+
+    for (int i = 0; i < input.length(); i++)
+    {
+        zalgoLine += input[i];
+        zalgoLine += getDiacritic(0, aboveCount);
+        zalgoLine += getDiacritic(1, middleCount);
+        zalgoLine += getDiacritic(3, belowCount);
+    }
+    return zalgoLine;
 }
